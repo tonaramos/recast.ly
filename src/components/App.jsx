2901553f0b2937ae029cd77b71ad4d7124f8a471
tonaramos <gtonaramos@gmail.com>
2018-05-09 19:16:51 -0700

    Refactored App to ES6 class, and established state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
-var App = (props) => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    
+    this.state = {
+      videoData: this.props.data,
+      videoPlaying: this.props.data[0]
+    };
+  }
+ 
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.videoPlaying} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videoData} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={props.data[0]} />
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={props.data} />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }  
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
