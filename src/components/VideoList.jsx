1ea3aa51db83a94c10178693159f592dba767edf
tonaramos <gtonaramos@gmail.com>
2018-05-09 17:13:45 -0700

    Feeding example data into the App, and using it to render the video list items

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,28 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div className="video-list">
+      {props.data.map( (video, i) => 
+        <VideoListEntry video={video} key={i} />
+      )}
+    </div>
   </div>
 );
+
+// class VideoList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+  
+//   render() {
+//     return (
+//       <div className="video-list">
+//         {exampleVideoData.map( video => 
+//           <VideoListEntry video={video} />
+//         )}
+//       </div>
+//     )
+//   }
+// }
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +33,30 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+
+
+
+
+// class TodoListItem extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <li>{this.props.todo}</li>
+//     );
+
+//   }
+
+// }
+
+// var TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(todo =>
+//       <TodoListItem todo={todo} />
+//     )}
+//   </ul>
+// );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 // TODO: Render the `App` component to the DOM
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App data={window.exampleVideoData} />, document.getElementById('app'));
